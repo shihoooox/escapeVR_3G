3ea0a1929e5dc2513bb4b0eb8a6c189649f7ea63
aaaaaepp1 <okamoto@okamotonaoki-no-Mac-mini.local>
2016-08-13 13:37:51 +0900

    VR対応完了

--- unityProject/escapeVR/Assets/Scripts/MainManager/MainManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/MainManager.cs
@@ -32,14 +32,15 @@ public class MainManager : MonoBehaviour {
 		RaycastHit hit_;
 		if (Physics.Raycast (ray_, out hit_, 100.0f)) {
 			GameObject tmp = hit_.collider.gameObject;
-			//Debug.Log (tmp.name + " " + tmp.transform.position);
+			Debug.Log (tmp.name + " " + tmp.transform.position);
 		}
 
 
 		//key: W (Wキーを押すと選択される処理)
 		if (Input.GetKeyDown (KeyCode.W)) {
+			Debug.Log ("W pressed");
 			Vector3 pos = new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0);
-			Ray ray = Camera.main.ScreenPointToRay (pos);
+			Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit, 100.0f)) {
 				int type = -1; //衝突したobjectType
