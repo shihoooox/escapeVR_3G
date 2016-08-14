18c1bb29f7e9f31de1b75934c58831181d2b774f
shiho <mlut4h@gmail.com>
2016-08-15 00:35:06 +0900

    訂正

--- unityProject/escapeVR/Assets/Scripts/MainManager/ObjectOnGame/ObjectOnGameManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/ObjectOnGame/ObjectOnGameManager.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class ObjectOnGameManager : MonoBehaviour {
 
 	public List<GameObject> itemList;
+	public GameObject tmp1;//アイテム数だけある　addする
+	public GameObject tmp2;
+	public GameObject tmp3;
+	public GameObject tmp4;
+	public GameObject tmp5;
 
 	// Use this for initialization
 	void Start () {
+		itemList.Add (tmp1);
+		itemList.Add (tmp2);
+		itemList.Add (tmp3);
+		itemList.Add (tmp4);
+		itemList.Add (tmp5);
+
 	}
 	
 	// Update is called once per frame
