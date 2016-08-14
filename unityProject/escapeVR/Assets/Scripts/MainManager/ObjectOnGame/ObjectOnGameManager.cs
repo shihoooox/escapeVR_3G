c0eacede8764914e0ea59aeff671b8b50b50071e
shiho <mlut4h@gmail.com>
2016-08-14 11:22:11 +0900

    ObjectOnGameManagerの制作

---
+++ unityProject/escapeVR/Assets/Scripts/MainManager/ObjectOnGame/ObjectOnGameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectOnGameManager : MonoBehaviour {
+
+	public List<GameObject> itemList;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// objectTypeと対応するgameObjectのactOnDetail(actNum)を実行する
+	public void motion(int objectType, int actNum){
+		ObjectOnGame tmp = this.GetComponent<ObjectOnGame>();
+		//tmp.objectType = objectType;
+		tmp.actOnDetail(actNum);		
+	}
+}
