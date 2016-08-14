98ccad66a6720eeb3e52af97d6fa4b0dc08d2da6
shiho <mlut4h@gmail.com>
2016-08-14 11:43:42 +0900

    変更

--- unityProject/escapeVR/Assets/Scripts/MainManager/ObjectOnGame/ObjectOnGame.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/ObjectOnGame/ObjectOnGame.cs
@@ -29,87 +29,87 @@ public class ObjectOnGame : MonoBehaviour {
 			iron.forInstanceMotion(objectType, actNum);
 			break;
 		case 4:
-			WoodenStick ws = this.GetComponent<Iron>();
+			WoodenStick ws = this.GetComponent<WoodenStick>();
 			ws.forInstanceMotion(objectType, actNum);
 			break;
 		case 5:
-			Ice ice = this.GetComponent<Iron>();
+			Ice ice = this.GetComponent<Ice>();
 			ice.forInstanceMotion(objectType, actNum);
 			break;
 		case 6:
-			Cup cup = this.GetComponent<Iron>();
+			Cup cup = this.GetComponent<Cup>();
 			cup.forInstanceMotion(objectType, actNum);
 			break;
 		case 7:
-			String str = this.GetComponent<Iron>();
+			String str = this.GetComponent<String>();
 			str.forInstanceMotion(objectType, actNum);
 			break;
 		case 8:
-			Lighter lig = this.GetComponent<Iron>();
+			Lighter lig = this.GetComponent<Lighter>();
 			lig.forInstanceMotion(objectType, actNum);
 			break;
 		case 9:
-			Hammer hammer = this.GetComponent<Iron>();
+			Hammer hammer = this.GetComponent<Hammer>();
 			hammer.forInstanceMotion(objectType, actNum);
 			break;
 		case 10:
-			Key key = this.GetComponent<Iron>();
+			Key key = this.GetComponent<Key>();
 			key.forInstanceMotion(objectType, actNum);
 			break;
 		case 11:
-			AlcoholInCup aic = this.GetComponent<Iron>();
+			AlcoholInCup aic = this.GetComponent<AlcoholInCup>();
 			aic.forInstanceMotion(objectType, actNum);
 			break;
 		case 12:
-			Candle candle = this.GetComponent<Iron>();
+			Candle candle = this.GetComponent<Candle>();
 			candle.forInstanceMotion(objectType, actNum);
 			break;
 		case 13:
-			Plate plate = this.GetComponent<Iron>();
+			Plate plate = this.GetComponent<Plate>();
 			plate.forInstanceMotion(objectType, actNum);
 			break;
 		case 14:
-			BilliardCue bc = this.GetComponent<Iron>();
+			BilliardCue bc = this.GetComponent<BilliardCue>();
 			bc.forInstanceMotion(objectType, actNum);
 			break;
 		case 15:
-			BilliardBall bb = this.GetComponent<Iron>();
+			BilliardBall bb = this.GetComponent<BilliardBall>();
 			bb.forInstanceMotion(objectType, actNum);
 			break;
 		case 16:
-			RefrigeratorDoor rf = this.GetComponent<Iron>();
+			RefrigeratorDoor rf = this.GetComponent<RefrigeratorDoor>();
 			rd.forInstanceMotion(objectType, actNum);
 			break;
 		case 17:
-			Chair01 chair01 = this.GetComponent<Iron>();
+			Chair01 chair01 = this.GetComponent<Chair01>();
 			chair01.forInstanceMotion(objectType, actNum);
 			break;
 		case 18:
-			Keyhole kh = this.GetComponent<Iron>();
+			Keyhole kh = this.GetComponent<Keyhole>();
 			kh.forInstanceMotion(objectType, actNum);
 			break;
 		case 19:
-			PasswordLocker pl = this.GetComponent<Iron>();
+			PasswordLocker pl = this.GetComponent<PasswordLocker>();
 			pl.forInstanceMotion(objectType, actNum);
 			break;
 		case 20:
-			シャッター ** = this.GetComponent<Iron>();
+			シャッター ** = this.GetComponent<シャッター>();
 			**.forInstanceMotion(objectType, actNum);
 			break;
 		case 21:
-			ElevatorButton eb = this.GetComponent<Iron>();
+			ElevatorButton eb = this.GetComponent<ElevatorButton>();
 			eb.forInstanceMotion(objectType, actNum);
 			break;
 		case 22:
-			ElevatorButton eb = this.GetComponent<Iron>();
+			ElevatorButton eb = this.GetComponent<ElevatorButton>();
 			eb.forInstanceMotion(objectType, actNum);
 			break;
 		case 23:
-			ElevatorButton eb = this.GetComponent<Iron>();
+			ElevatorButton eb = this.GetComponent<ElevatorButton>();
 			eb.forInstanceMotion(objectType, actNum);
 			break;
 		case 37:
-			ElevatorDoor ed = this.GetComponent<Iron>();
+			ElevatorDoor ed = this.GetComponent<ElevatorDoor>();
 			ed.forInstanceMotion(objectType, actNum);
 			break;
 		default:
