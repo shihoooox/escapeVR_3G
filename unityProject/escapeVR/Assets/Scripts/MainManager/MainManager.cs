0e7a474f9abccab4b17623c82519e172cdd66fa9
Naoki <epp1.ae.cap@gmail.comm>
2016-08-15 13:47:47 +0900

    今から合成処理頑張るぞ

--- unityProject/escapeVR/Assets/Scripts/MainManager/MainManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/MainManager.cs
@@ -15,18 +15,30 @@ public class MainManager : MonoBehaviour {
 	void Update () {
 		//key: Q (Qキーを押すとメニューが出て話すと上に消える処理)
 		if (Input.GetKeyDown (KeyCode.Q)) {
-			MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> ();
-			SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager> ();
-			MomManager.moveToScreen (true);
-			SomManager.moveToScreen (true);
+			this.pressKeyDown_Q ();
 		} else if (Input.GetKeyUp (KeyCode.Q)) {
-			MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> ();
-			SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager> ();
-			MomManager.moveToScreen (false);
-			SomManager.moveToScreen (false);
+			this.pressKeyUp_Q ();
 		}
 		
  		//each frame
+		this.eachFrame();
+
+		//key: W (Wキーを押すと選択される処理)
+		if (Input.GetKeyDown (KeyCode.W)) {
+			this.pressKeyDown_W ();
+		}
+
+		//key:E (Eキーを押すとオブジェクトが消えるデバッグ)
+		if (Input.GetKeyDown (KeyCode.E)) {
+			this.pressKeyDown_E ();
+		}
+	}
+
+
+
+
+
+	void eachFrame() {
 		Vector3 pos_ = new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0);
 		Ray ray_ = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
 		RaycastHit hit_;
@@ -39,41 +51,58 @@ public class MainManager : MonoBehaviour {
 			}
 		}
 		MarkerManager_G.GetComponent<MarkerManager> ().focus (MarkerObjectType);
+	}
 
-		//key: W (Wキーを押すと選択される処理)
-		if (Input.GetKeyDown (KeyCode.W)) {
-			Debug.Log ("W pressed");
-			Vector3 pos = new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0);
-			Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit, 100.0f)) {
-				int type = -1; //衝突したobjectType
+	void pressKeyDown_Q() {
+		MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> ();
+		SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager> ();
+		MomManager.moveToScreen (true);
+		SomManager.moveToScreen (true);
+	}
 
-				//hitしたのがMainObjectMenu系なら
-				if (hit.collider.gameObject.tag == "MainObjectMenuFrame") {
-					MainObjectMenuFrame frame = hit.collider.gameObject.GetComponent<MainObjectMenuFrame> (); //objectTypeを知るためにframeのインスタンス取得
-					MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> (); //managerのインスタンス取得
-					SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager>();
-					type = frame.objectType;
-					Debug.Log ("P_key pressed, hit type : " + type);
-					if (type > 0) {
-						if (!frame.isSelected) {
-							MomManager.indicateSelected (type);
-							SomManager.indicateSelected (type+100);
-						} else {
-							MomManager.showObjectsDetail (type);
-						}
+	void pressKeyUp_Q() {
+		MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> ();
+		SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager> ();
+		MomManager.moveToScreen (false);
+		SomManager.moveToScreen (false);
+	}
+
+	void pressKeyDown_W() {
+		Debug.Log ("W pressed");
+		Vector3 pos = new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0);
+		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100.0f)) {
+			int type = -1; //衝突したobjectType
+
+			//hitしたのがMainObjectMenu系なら
+			if (hit.collider.gameObject.tag == "MainObjectMenuFrame") {
+				MainObjectMenuFrame frame = hit.collider.gameObject.GetComponent<MainObjectMenuFrame> (); //objectTypeを知るためにframeのインスタンス取得
+				MainObjectMenuManager MomManager = MainObjectMenuManager_G.GetComponent<MainObjectMenuManager> (); //managerのインスタンス取得
+				SubObjectMenuManager SomManager = SubObjectMenuManager_G.GetComponent<SubObjectMenuManager>();
+				type = frame.objectType;
+				Debug.Log ("P_key pressed, hit type : " + type);
+				if (type > 0) {
+					if (!frame.isSelected) {
+						MomManager.indicateSelected (type);
+						SomManager.indicateSelected (type+100);
+					} else {
+						MomManager.showObjectsDetail (type);
 					}
 				}
-				//hitしたのがMarkerなら
-				else if(hit.collider.gameObject.tag == "Marker") {
-					MarkerInstance instance = hit.collider.gameObject.GetComponent<MarkerInstance> ();
-					MarkerManager manager =  MarkerManager_G.GetComponent<MarkerManager> ();
-					type = instance.objectType;
-					Debug.Log("move to " + type);
-					manager.moveTo (type);
-				}
+			}
+			//hitしたのがMarkerなら
+			else if(hit.collider.gameObject.tag == "Marker") {
+				MarkerInstance instance = hit.collider.gameObject.GetComponent<MarkerInstance> ();
+				MarkerManager manager =  MarkerManager_G.GetComponent<MarkerManager> ();
+				type = instance.objectType;
+				Debug.Log("move to " + type);
+				manager.moveTo (type);
 			}
 		}
+	}
+
+	void pressKeyDown_E() {
+
 	}
 }
