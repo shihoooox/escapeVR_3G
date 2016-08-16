e8d1ff319f78d4cd88e909e31c8812ce5a59c8da
aaaaaepp1 <epp1.ae.cap@gmail.com>
2016-08-17 02:23:30 +0900

    テクスチャ全部Ｄ＆Ｄした・・

--- unityProject/escapeVR/Assets/Scripts/MainManager/Textures/TextureManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/Textures/TextureManager.cs
@@ -3,20 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class TextureManager : MonoBehaviour {
-	private List<Texture> menu_normal;
+	public Texture[] menu_normal;
 	// Use this for initialization
 	void Start () {
-		/*
-		string currentDir = System.IO.Directory.GetCurrentDirectory ();
-		Debug.Log (currentDir);
-		string menu_normal_texPath = currentDir + "\\Assets\\Texture\\menu_normal";
-		string[] tmpstr = System.IO.Directory.GetFiles (menu_normal_texPath);
-		for (int i = 0; i < tmpstr.Length; i++) {
-			if(tmpstr[i].IndexOf(".meta") == -1) Debug.Log (tmpstr [i]);
-		}
-		*/
-		System.OperatingSystem os = System.Environment.OSVersion;
-		Debug.Log (os.ToString());
+		
+
+
 	}
 	
 	// Update is called once per frame
