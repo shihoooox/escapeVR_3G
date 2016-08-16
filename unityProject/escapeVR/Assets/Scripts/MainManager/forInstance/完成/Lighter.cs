2847b67323c4b5351534b8e073953303e8f78fba
shiho <mlut4h@gmail.com>
2016-08-16 23:42:09 +0900

    回転系終わり

---
+++ unityProject/escapeVR/Assets/Scripts/MainManager/forInstance/完成/Lighter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lighter : MonoBehaviour {
+
+	// OM_2もつけてね
+
+	public GameObject particle;
+	public GameObject huta;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void forInstanceMotion(int actNum){
+		if(actNum == 1) {
+			// 火がつく処理(パーティクルをONにする)
+			ParticleSystem ps = particle.GetComponent<ParticleSystem>();
+			ps.Play();
+			return;
+		} else if(actNum == 2){
+			//	蓋が開く処理
+			Transform in_target;
+			in_target = huta.transform;
+			in_target.transform.Rotate (-135, 0, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(huta, in_target);
+			return;
+		} else if(actNum == 3){
+			//	蓋が閉じる処理
+			// 火が消える処理(パーティクルをOFFにする)
+			ParticleSystem ps = particle.GetComponent<ParticleSystem>();
+			ps.Stop();
+			Transform in_target;
+			in_target = huta.transform;
+			in_target.transform.Rotate (135, 0, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(huta, in_target);
+			return;
+		}
+	}
+}
---
+++ unityProject/escapeVR/Assets/Scripts/MainManager/forInstance/完成/RefrigeratorDoor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefrigeratorDoor : MonoBehaviour {
+
+	// OM_2もつけてね
+
+	public GameObject doorUM; // ドア上右の回転軸になるemptyを入れて
+	public GameObject doorUH; // ドア上左の回転軸になるemptyを入れて
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void forInstanceMotion(int actNum){	
+		if(actNum == 1) {
+			//ドア上右が開く処理
+			Transform in_target;
+			in_target = doorUM.transform;
+			in_target.transform.Rotate (0, -150, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(doorUM, in_target);
+			return;
+		}else if(actNum == 2) {
+			//ドア上右が閉じる処理
+			Transform in_target;
+			in_target = doorUM.transform;
+			in_target.transform.Rotate (0, 150, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(doorUM, in_target);
+			return;
+		}else if(actNum == 3) {
+			//ドア上右が開く処理
+			Transform in_target;
+			in_target = doorUH.transform;
+			in_target.transform.Rotate (0, 150, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(doorUH, in_target);
+			return;
+		}else if(actNum == 4) {
+			//ドア上右が閉じる処理
+			Transform in_target;
+			in_target = doorUH.transform;
+			in_target.transform.Rotate (0, -150, 0);
+			ObjectMover_2 om2 = this.GetComponent<ObjectMover_2>();
+			om2.startMoving(doorUH, in_target);
+			return;
+		}
+	}
+}
