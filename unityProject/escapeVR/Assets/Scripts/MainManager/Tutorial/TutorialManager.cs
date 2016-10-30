97a20d119e20ee45a0ef42b99539b7bf87389568
ioshin <chikke.black@gmail.com>
2016-10-30 13:21:39 +0900

    nextメソッドの制作

--- unityProject/escapeVR/Assets/Scripts/MainManager/Tutorial/TutorialManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/Tutorial/TutorialManager.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TutorialManager : MonoBehaviour {
 
+
+	public List<GameObject> dialogList;
+
 	// Use this for initialization
 	void Start () {
-	
+		//子を取得、リストに格納
+		Transform children = this.GetComponentInChildren<Transform> ();
+		foreach (Transform ob in children) {
+			dialogList.Add (ob.gameObject);
+		}
 	}
 
 	// Update is called once per frame
@@ -15,7 +23,20 @@ public class TutorialManager : MonoBehaviour {
 
 	public bool next(int dialogType) {
 		Debug.Log ("TutorialManager, next()は未完成です");
-
-		return false;
+		if(dialogType>=401&&dialogType<=406){
+			if(dialogType==401){
+				dialogList[0].show (true);
+				return true;
+			}if (dialogType == 406) {
+				
+				return true;
+			} else {
+				dialogList [dialogType - 402].show (false);
+				dialogList [dialogType - 401].show (true);
+				return true;
+			}
+		}else {
+			return false;
+		}
 	}
 }
