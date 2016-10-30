f77765aa1934c4887ec2cdf8e0689af56dde40ad
ioshin <chikke.black@gmail.com>
2016-10-30 10:05:36 +0900

    showメソッド制作(未完)

---
+++ unityProject/escapeVR/Assets/Scripts/MainManager/Tutorial/TutorialDialog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialDialog : MonoBehaviour {
+
+	int dialogType;
+	float period;
+	float amplitude;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//自身をフェードインさせたりフェードアウトさせたりする
+	void show () {
+
+	}
+}
