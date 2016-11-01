e102bc823150b20ea4fb0b26dc07adf0cf4e95d8
aaaaaepp1 <epp1.ae.cap@gmail.com>
2016-11-01 15:43:35 +0900

    Tutorial関連組み込み完了

--- unityProject/escapeVR/Assets/Scripts/MainManager/Tutorial/TutorialManager.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/Tutorial/TutorialManager.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TutorialManager : MonoBehaviour {
 
+
+	public List<GameObject> dialogList;
+
 	// Use this for initialization
 	void Start () {
-	
+		//子を取得、リストに格納
+		Transform children = this.GetComponentInChildren<Transform> ();
+		foreach (Transform ob in children) {
+			dialogList.Add (ob.gameObject);
+		}
 	}
 
 	// Update is called once per frame
@@ -14,8 +22,38 @@ public class TutorialManager : MonoBehaviour {
 	}
 
 	public bool next(int dialogType) {
-		Debug.Log ("TutorialManager, next()は未完成です");
+	//	Debug.Log ("TutorialManager, next()は未完成です");
 
-		return false;
+		TutorialDialog tar,tar2;  //showを使う時に呼ぶDialogオブジェクト、tar2は前のオブジェクト
+		tar = null;
+		tar2 = null;
+
+
+		if(dialogType>=401&&dialogType<=406){
+			foreach(GameObject ob in dialogList){
+				if(ob.GetComponent<TutorialDialog>().dialogType == dialogType){
+					tar = ob.GetComponent<TutorialDialog>();
+				}
+				if(ob.GetComponent<TutorialDialog>().dialogType == dialogType-1){
+					tar2 = ob.GetComponent<TutorialDialog>();
+				}
+			}
+			if(dialogType==401){
+				
+				tar.show (true);
+
+				return true;
+			}if (dialogType == 406) {
+				//opening.sceneに移行、未実装
+				Debug.Log ("TutorialManager.next(406)は未完成です");
+				return true;
+			} else {
+				tar2.show (false);
+				tar.show (true);
+				return true;
+			}
+		}else {
+			return false;
+		}
 	}
 }
