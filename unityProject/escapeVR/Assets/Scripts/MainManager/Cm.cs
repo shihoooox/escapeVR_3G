c06a3547703b72fa804678cc48430f5250e40435
aaaaaepp1 <epp1.ae.cap@gmail.com>
2016-11-03 00:33:36 +0900

    ポインタテクスチャ変更、EV内の照明変更、cameraBloom追加

--- unityProject/escapeVR/Assets/Scripts/MainManager/Cm.cs
+++ unityProject/escapeVR/Assets/Scripts/MainManager/Cm.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Cm : MonoBehaviour {
 	public GameObject c;
 	public GameObject rayPoint;
+	public float rayPoint_rotate_x;
+	public float rayPoint_rotate_y;
+	public float rayPoint_rotate_z;
 	public float pointDist;
 
 	// Use this for initialization
@@ -19,6 +22,6 @@ public class Cm : MonoBehaviour {
 	public void setRayPoint(Ray ray) {
 		rayPoint.transform.position = ray.GetPoint (pointDist);
 		rayPoint.transform.rotation = Camera.main.transform.rotation;
-		rayPoint.transform.Rotate (-45f, 0f, 0f);
+		rayPoint.transform.Rotate (rayPoint_rotate_x, rayPoint_rotate_y, rayPoint_rotate_z);
 	}
 }
